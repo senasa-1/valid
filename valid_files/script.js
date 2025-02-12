d83395ff4299b112003d01fdbc70d12976f2d99d
senasa-1 <testsenasa5@gmail.com>
2025-02-12 16:37:06 -0300

    Update script.js

--- valid_files/script.js
+++ valid_files/script.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Obtener los parámetros de la URL 
 	const params = new URLSearchParams(window.location.search); 
 	// Modificar el título si existe el parámetro 'titulo' 
-	if (params.has('cuve')) 
+	if (params.has('c')) 
 	{ 
-		document.getElementById('cuve').textContent = params.get('cuve'); 
+		document.getElementById('cuve').textContent = params.get('c'); 
 	} 
 	// Modificar el contenido si existe el parámetro 'contenido' 
-	if (params.has('fch')) 
+	if (params.has('f')) 
 	{ 
-		document.getElementById('fch').textContent = params.get('fch'); 
+		document.getElementById('fch').textContent = params.get('f'); 
 	} 
-	if (params.has('nro')) 
+	if (params.has('n')) 
 	{ 
-		document.getElementById('nro').textContent = params.get('nro'); 
+		document.getElementById('nro').textContent = params.get('n'); 
 	} 
-	if (params.has('ptr')) 
+	if (params.has('p')) 
 	{ 
-		document.getElementById('ptr').textContent = params.get('ptr'); 
+		document.getElementById('ptr').textContent = params.get('p'); 
 	} 
 });
